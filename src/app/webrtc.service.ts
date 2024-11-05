4ea3d2fd1e1337482350b5bad6cdb419941a5e41
Fatih YILDIZ <fth.yldz@outlook.com.tr>
2024-11-06 00:09:20 +0300

    .

--- src/app/webrtc.service.ts
+++ src/app/webrtc.service.ts
@@ -39,8 +39,10 @@ export class WebRtcService implements OnDestroy {
         try {
             this.pc = new RTCPeerConnection({
                 iceServers: [
-                    { urls: "stun:stun.services.mozilla.com" },
-                    { urls: "stun:stun.l.google.com:19302" }
+                    { url: 'stun:stun.l.google.com:19302' },
+                    { url: 'stun:stun1.l.google.com:19302' },
+                    { url: 'stun:stun2.l.google.com:19302' },
+                    { url: 'stun:stun3.l.google.com:19302' }
                 ]
             });
         } catch (error) {
@@ -61,12 +63,36 @@ export class WebRtcService implements OnDestroy {
         this.pc.ontrack = (event: any) => {
             this.remote.nativeElement.srcObject = event.streams[0];
         }
+
+        this.pc.oniceconnectionstatechange = (event: any) => {
+            console.log('ICE connection state: ', this.pc.iceConnectionState);
+            switch (this.pc.iceConnectionState) {
+                case 'connected':
+                    console.log('ICE connection established');
+                    break;
+                case 'failed':
+                    console.log('ICE connection failed');
+                    // Bağlantı başarısız olduysa, tekrar deneme veya hata mesajı
+                    break;
+                case 'disconnected':
+                    console.log('ICE connection disconnected');
+                    // Bağlantı kesildi, tekrar bağlanmaya çalışabiliriz
+                    break;
+                case 'closed':
+                    console.log('ICE connection closed');
+                    // Bağlantı kapandı
+                    break;
+                // Diğer durumlar da eklenebilir
+            }
+        };
     }
 
     public messageReceived(message: any) {
         const msg = JSON.parse(message);
         if (this.pc) {
             if (msg.msg_type == "candidate") {
+                console.log("Candidate geldi -> ", msg.content);
+
                 this.pc.addIceCandidate(new RTCIceCandidate({ candidate: msg.content, sdpMid: msg.sdp_mid, sdpMLineIndex: msg.sdp_mline_index, usernameFragment: msg.username_fragment }));
             } else if (msg.msg_type == "answer") {
                 this.callActive = true;
@@ -75,43 +101,43 @@ export class WebRtcService implements OnDestroy {
         }
     }
 
-   /*  public showMe() {
+    /*  public showMe() {
+         if (isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
+             navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+                 .then(stream => {
+                     this.localStream = stream;
+                     // Her bir track'i ekle
+                     stream.getTracks().forEach(track => {
+                         this.pc.addTrack(track, stream);
+                     });
+ 
+                     this.me.nativeElement.srcObject = stream;
+                 })
+                 .catch(error => {
+                     console.error("Media Device Hatası: ", error);
+                 });
+         }
+     } */
+
+    public async showMe(): Promise<void> {
         if (isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
-            navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-                .then(stream => {
-                    this.localStream = stream;
-                    // Her bir track'i ekle
-                    stream.getTracks().forEach(track => {
-                        this.pc.addTrack(track, stream);
-                    });
-
-                    this.me.nativeElement.srcObject = stream;
-                })
-                .catch(error => {
-                    console.error("Media Device Hatası: ", error);
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+                this.localStream = stream;
+                // Add each track to the peer connection
+                stream.getTracks().forEach(track => {
+                    this.pc.addTrack(track, this.localStream);
                 });
-        }
-    } */
 
-        public async showMe(): Promise<void> {
-            if (isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
-                try {
-                    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-                    this.localStream = stream;
-                    // Add each track to the peer connection
-                    stream.getTracks().forEach(track => {
-                        this.pc.addTrack(track, stream);
-                    });
-
-                    this.me.nativeElement.srcObject = stream;
-                } catch (error) {
-                    console.error("Media Device Error: ", error);
-                    throw error;
-                }
-            } else {
-                return Promise.reject("Not in browser or mediaDevices not available");
+                this.me.nativeElement.srcObject = this.localStream;
+            } catch (error) {
+                console.error("Media Device Error: ", error);
+                throw error;
             }
+        } else {
+            return Promise.reject("Not in browser or mediaDevices not available");
         }
+    }
 
 
     /* public showRemote() {
@@ -128,22 +154,22 @@ export class WebRtcService implements OnDestroy {
         }
     } */
 
-        public async showRemote() {
-            try {
-                await this.showMe(); // Wait for showMe() to complete
-        
-                const offer = await this.pc.createOffer();
-                console.log("SDP Offer:", offer.sdp); // Log the SDP offer
-                
-                await this.pc.setLocalDescription(offer);
-        
-                this.sendMessage.emit(JSON.stringify({ msg_type: "offer", content: this.pc.localDescription.sdp }));
-                this.callActive = true;
-            } catch (error) {
-                console.error("Error in showRemote: ", error);
-            }
+    public async showRemote() {
+        try {
+            await this.showMe(); // Wait for showMe() to complete
+
+            const offer = await this.pc.createOffer();
+            console.log("SDP Offer:", offer.sdp); // Log the SDP offer
+
+            await this.pc.setLocalDescription(offer);
+
+            this.sendMessage.emit(JSON.stringify({ msg_type: "offer", content: this.pc.localDescription.sdp }));
+            this.callActive = true;
+        } catch (error) {
+            console.error("Error in showRemote: ", error);
         }
-        
+    }
+
 
     public hangup() {
         this.pc.close();
